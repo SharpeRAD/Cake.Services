fceec643cf9572f659c414703b17746bdeafcbe5
SharpeRAD <code@sharperad.com>
2016-07-23 11:43:15 +0100

    Update build script to use tools path environment variables

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("Cake.Services")]
-[assembly: AssemblyVersion("0.2.1")]
-[assembly: AssemblyFileVersion("0.2.1")]
-[assembly: AssemblyInformationalVersion("0.2.1")]
+[assembly: AssemblyVersion("0.2.2")]
+[assembly: AssemblyFileVersion("0.2.2")]
+[assembly: AssemblyInformationalVersion("0.2.2")]
 [assembly: AssemblyCopyright("Copyright (c) 2015 - 2016 Phillip Sharpe")]
 
