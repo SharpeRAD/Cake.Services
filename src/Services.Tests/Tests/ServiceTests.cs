c7dc0bc38d323e0844fdd7066888318d0a87da12
Nelson Wells <nelsonwellswku@gmail.com>
2017-04-19 21:38:19 -0500

    Resolves #7; sc install args are built correctly

--- src/Services.Tests/Tests/ServiceTests.cs
+++ src/Services.Tests/Tests/ServiceTests.cs
@@ -12,52 +12,76 @@
     using Cake.Core.Diagnostics;
     using Cake.Core.IO;
     using Cake.Powershell;
+
 #endregion
 
 
 
 namespace Cake.Services.Tests
 {
-    public class ServiceTests
-    {
-        [Fact]
-        public void Should_Service_IsInstalled()
-        {
-            IServiceManager manager = CakeHelper.CreateServiceManager();
+	public class ServiceTests
+	{
+		[Fact]
+		public void Should_Service_IsInstalled()
+		{
+			IServiceManager manager = CakeHelper.CreateServiceManager();
 
-            bool result1 = manager.IsInstalled("MpsSvc");
-            bool result2 = manager.IsInstalled("TestSer");
+			bool result1 = manager.IsInstalled("MpsSvc");
+			bool result2 = manager.IsInstalled("TestSer");
 
-            Assert.True(result1);
-            Assert.False(result2);
-        }
+			Assert.True(result1);
+			Assert.False(result2);
+		}
 
-        [Fact]
-        public void Should_Get_Service()
-        {
-            IServiceManager manager = CakeHelper.CreateServiceManager();
+		[Fact]
+		public void Should_Get_Service()
+		{
+			IServiceManager manager = CakeHelper.CreateServiceManager();
 
-            ServiceController controller = manager.GetService("MpsSvc");
+			ServiceController controller = manager.GetService("MpsSvc");
 
-            Assert.True(controller != null, "Check Rights");
-        }
+			Assert.True(controller != null, "Check Rights");
+		}
 
-        [Fact]
-        public void Should_Change_Service_State()
-        {
-            IServiceManager manager = CakeHelper.CreateServiceManager();
-            bool result = false;
+		[Fact]
+		public void Should_Change_Service_State()
+		{
+			IServiceManager manager = CakeHelper.CreateServiceManager();
+			bool result = false;
 
-            if (manager.IsRunning("MpsSvc"))
-            {
-                result = manager.Stop("MpsSvc");
-            }
-            else
-            {
-                result = manager.Start("MpsSvc");
-            }
+			if (manager.IsRunning("MpsSvc"))
+			{
+				result = manager.Stop("MpsSvc");
+			}
+			else
+			{
+				result = manager.Start("MpsSvc");
+			}
 
-            Assert.True(result, "Check Rights");
-        }
-    }
+			Assert.True(result, "Check Rights");
+		}
+
+		[Fact]
+		public void Should_Construct_Arg_String()
+		{
+			ServiceManager manager = (ServiceManager) CakeHelper.CreateServiceManager();
+
+			var argumentBuilder = manager.CreateInstallArguments("", new InstallSettings()
+			{
+				ServiceName = "TestService",
+				ExecutablePath = @"C:\my\path\to\bin.exe",
+				DisplayName = "Test Service Display Name",
+				Dependencies = "TestDependencies",
+				Username = "TestUsername",
+				Password = "TestPasswordPassword",
+				StartMode = "TestStartMode"
+			});
+
+			var actual = argumentBuilder.Render();
+
+			var expected = @"""TestService"" binPath= ""C:/my/path/to/bin.exe"" DisplayName= ""Test Service Display Name"" depend= ""TestDependencies"" start= ""TestStartMode"" obj= ""TestUsername"" password= ""TestPasswordPassword""";
+
+			Assert.Equal(expected, actual);
+		}
+	}
 }
